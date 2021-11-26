c0ffee539a7606b5efff46014bd3b63263936c64
Mark <mverleg.noreply@gmail.com>
2021-11-26 21:39:35 +0100

    Exmapple for 2020dec04

--- 2020/src/dec04.rs
+++ 2020/src/dec04.rs
@@ -2,26 +2,38 @@ use ::std::fs::read_to_string;
 use std::collections::HashSet;
 
 pub fn dec04a() {
-    do_batch()
+    println!("{}", do_batch());
 }
 
-fn do_batch() {
+fn do_batch() -> u32 {
+    let mut count = 0;
     let mut current = String::with_capacity(1024);
     for line in read_to_string("dec04ex.txt").unwrap().lines() {
         if line.is_empty() {
-            do_entry(&current);
+            if do_entry(&current) {
+                count += 1;
+            }
             current.clear();
             continue
         }
         current.push_str(" ");
         current.push_str(line);
     }
+    count
 }
 
-fn do_entry(inp: &str) {
+fn do_entry(inp: &str) -> bool {
+    let mut expected = HashSet::new();
+    expected.insert("byr".to_owned());
+    expected.insert("iyr".to_owned());
+    expected.insert("eyr".to_owned());
+    expected.insert("hgt".to_owned());
+    expected.insert("hcl".to_owned());
+    expected.insert("ecl".to_owned());
+    expected.insert("pid".to_owned());
     let keys = inp.split(' ')
         .map(|s| s.chars().take(3).collect::<String>())
         .filter(|s| !s.is_empty())
         .collect::<HashSet<_>>();
-    println!("keys = {:?}", &keys);
+    expected.is_subset(&keys)
 }
