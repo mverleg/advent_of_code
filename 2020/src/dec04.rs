c0ffeedfbddcf4e9f59c061134cfecb1af2d1a8a
Mark <mverleg.noreply@gmail.com>
2021-11-26 21:51:27 +0100

    First valudations for part 2 for 2020dec04

--- 2020/src/dec04.rs
+++ 2020/src/dec04.rs
@@ -2,20 +2,21 @@ use ::std::fs::read_to_string;
 use std::collections::HashSet;
 
 pub fn dec04a() {
-    println!("{}", do_batch());
+    println!("4a: {}", do_batch(has_all_fields));
+    println!("4b: {}", do_batch(all_fields_valid));
 }
 
 /// Make sure the input ends in newlines
-fn do_batch() -> u32 {
+fn do_batch(validate: fn(&str) -> bool) -> u32 {
     let mut count = 0;
     let mut current = String::with_capacity(1024);
     for line in read_to_string("dec04.txt").unwrap().lines() {
         if line.is_empty() {
-            if do_entry(&current) {
+            if validate(&current) {
                 count += 1;
             }
             current.clear();
-            continue
+            continue;
         }
         current.push_str(" ");
         current.push_str(line);
@@ -23,7 +24,7 @@ fn do_batch() -> u32 {
     count
 }
 
-fn do_entry(inp: &str) -> bool {
+fn has_all_fields(inp: &str) -> bool {
     let mut expected = HashSet::new();
     expected.insert("byr".to_owned());
     expected.insert("iyr".to_owned());
@@ -37,10 +38,48 @@ fn do_entry(inp: &str) -> bool {
         .filter(|s| !s.is_empty())
         .collect::<HashSet<_>>();
     if expected.is_subset(&keys) {
-        println!("valid: {:?}", &keys);
+        // println!("valid: {:?}", &keys);
         true
     } else {
-        println!("INvalid: {:?}", &keys);
+        // println!("INvalid: {:?}", &keys);
         false
     }
+}
+
+fn all_fields_valid(inp: &str) -> bool {
+    if !has_all_fields(inp) {
+        return false;
+    }
+    for field in inp.split(' ') {
+        if field.is_empty() {
+            continue
+        }
+        let value = field.chars().skip(4).collect::<String>();
+        if field.starts_with("byr") {
+            if !value.parse::<u32>().map(|yr| yr >= 1920 && yr <= 2002).unwrap_or_else(|_| false) {
+                return false
+            }
+            //println!("valid byr: {}", &value);
+        } else if field.starts_with("iyr") {
+            if !value.parse::<u32>().map(|yr| yr >= 2010 && yr <= 2020).unwrap_or_else(|_| false) {
+                return false
+            }
+            //println!("valid iyr: {}", &value);
+        } else if field.starts_with("eyr") {
+            return false;  //TODO @mark:
+        } else if field.starts_with("hgt") {
+            return false;  //TODO @mark:
+        } else if field.starts_with("hcl") {
+            return false;  //TODO @mark:
+        } else if field.starts_with("ecl") {
+            return false;  //TODO @mark:
+        } else if field.starts_with("pid") {
+            return false;  //TODO @mark:
+        } else if field.starts_with("cid") {
+            continue;
+        } else {
+            panic!("{}", field);
+        }
+    }
+    true
 }
