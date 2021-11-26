c0ffeea9159ca37481dede886a13c02a42d85f75
Mark <mverleg.noreply@gmail.com>
2021-11-26 01:03:54 +0100

    Solve part 2 of 2020dec01 by kinda brute forcing it

--- 2020/01/src/main.rs
+++ 2020/01/src/main.rs
@@ -1,19 +1,13 @@
 use ::std::fs::read_to_string;
 
 fn main() {
-    let res = run();
+    let res = run2();
     println!("{}", res);
 }
 
-fn run() -> u32 {
+fn run1() -> u32 {
     let goal = 2020;
-    let mut yrs = read_to_string("input.txt")
-        .unwrap()
-        .lines()
-        .filter(|ln| !ln.is_empty())
-        .map(|ln| ln.parse::<u32>().unwrap())
-        .collect::<Vec<_>>();
-    yrs.sort();
+    let yrs = read_sorted();
     let mut i2 = yrs.len() - 1;
     for yr1 in &yrs {
         println!("compare {} and {}", yr1, yrs[i2]);
@@ -25,4 +19,37 @@ fn run() -> u32 {
         }
     }
     panic!("unreachable if there is a solution")
+}
+
+fn run2() -> u32 {
+    let goal = 2020;
+    let yrs = read_sorted();
+    for yr1 in yrs.iter().rev() {
+        for yr2 in &yrs {
+            if yr1 + yr2 > goal {
+                break
+            }
+            for yr3 in &yrs {
+                if yr1 + yr2 + yr3 > goal {
+                    break
+                }
+                println!("compare {}, {}, {}", yr1, yr2, yr3);
+                if yr1 + yr2 + yr3 == goal {
+                    return yr1 * yr2 * yr3;
+                }
+            }
+        }
+    }
+    panic!("unreachable if there is a solution")
+}
+
+fn read_sorted() -> Vec<u32> {
+    let mut yrs = read_to_string("input.txt")
+        .unwrap()
+        .lines()
+        .filter(|ln| !ln.is_empty())
+        .map(|ln| ln.parse::<u32>().unwrap())
+        .collect::<Vec<_>>();
+    yrs.sort();
+    yrs
 }
