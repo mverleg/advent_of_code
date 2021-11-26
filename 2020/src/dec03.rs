c0ffeefee9bfc3b481e58ad411a14f09c9c5299b
Mark <mverleg.noreply@gmail.com>
2021-11-26 21:21:33 +0100

    First attempt at part 2 of 2020dec03

--- 2020/src/dec03.rs
+++ 2020/src/dec03.rs
@@ -1,14 +1,28 @@
 use ::std::fs::read_to_string;
 
 pub fn dec03a() {
+    let res = vec![
+        descend(1, 1),
+        descend(3, 1),
+        descend(5, 1),
+        descend(7, 1),
+        descend(1, 2),
+    ];
+    res.iter().for_each(|r| print!("{} x ", r));
+    let prod = res.into_iter().reduce(|a, b| a * b).unwrap();
+    println!("1 = {}", prod);
+}
+
+fn descend(dx: usize, dy: usize) -> usize {
+    assert!(dy > 0);
     let mut count = 0;
-    let mut col = 4;
+    let mut col = 1 + dx;
     let content = read_to_string("dec03.txt").unwrap();
     let mut lines = content.lines();
     let col_cnt = lines.next().unwrap().len();
     let mut line_cnt = 1;
-    println!("{}", col_cnt);  //TODO @mark: TEMPORARY! REMOVE THIS!
-    for line in lines {
+//    println!("{}", col_cnt);  //TODO @mark: TEMPORARY! REMOVE THIS!
+    for line in lines.step_by(dy) {
         assert!(col_cnt == line.len());
         line_cnt += 1;
         let prev = (col + col_cnt - 1) % col_cnt;
@@ -16,14 +30,14 @@ pub fn dec03a() {
         if maybe_tree == '#' {
             count += 1;
         }
-        println!("{} / {} = {}", line, prev + 1, maybe_tree);  //TODO @mark: TEMPORARY! REMOVE THIS!
-        if maybe_tree == '#' {
-            println!("{}# {}", " ".repeat(prev), count);
-        } else {
-            println!("{}^ {}", " ".repeat(prev), count);
-        }
+//        println!("{} / {} = {}", line, prev + 1, maybe_tree);  //TODO @mark: TEMPORARY! REMOVE THIS!
+//        if maybe_tree == '#' {
+//            println!("{}# {}", " ".repeat(prev), count);
+//        } else {
+//            println!("{}^ {}", " ".repeat(prev), count);
+//        }
         col += 3;
     }
     assert!(count < line_cnt);
-    println!("{}", count)
+    count
 }
