c0ffee7242d8da3642d3ca470818242a36c2887a
Mark <mverleg.noreply@gmail.com>
2021-11-26 01:50:00 +0100

    Part 2 of 2020dec02

--- 2020/src/dec02.rs
+++ 2020/src/dec02.rs
@@ -1,4 +1,5 @@
 use ::std::fs::read_to_string;
+use std::ops::BitXor;
 use ::lazy_static::lazy_static;
 use ::regex::Regex;
 
@@ -7,14 +8,24 @@ lazy_static! {
 }
 
 pub fn dec02a() {
+    let valid_cnt = process_file(validate_cnt);
+    println!("{}", valid_cnt);
+}
+
+pub fn dec02b() {
+    let valid_cnt = process_file(validate_pos);
+    println!("{}", valid_cnt);
+}
+
+fn process_file(validate: fn(&Pwd) -> bool) -> usize {
     let valid_cnt = read_to_string("dec02.txt")
         .unwrap()
         .lines()
         .filter(|ln| !ln.is_empty())
         .map(|ln| parse(ln))
-        .filter(|pwd| validate(pwd))
+        .filter(|pwd| validate_pos(pwd))
         .count();
-    println!("{}", valid_cnt);
+    valid_cnt
 }
 
 #[derive(Debug)]
@@ -35,9 +46,16 @@ fn parse(line: &str) -> Pwd {
     }
 }
 
-fn validate(pwd: &Pwd) -> bool {
+fn validate_cnt(pwd: &Pwd) -> bool {
     let cnt = pwd.pass.chars()
         .filter(|c| c == &pwd.letter)
         .count() as u32;
-    return cnt >= pwd.min && cnt <= pwd.max;
+    cnt >= pwd.min && cnt <= pwd.max
+}
+
+fn validate_pos(pwd: &Pwd) -> bool {
+    let passchars = pwd.pass.chars().collect::<Vec<char>>();
+    let first_match = passchars[pwd.min as usize - 1] == pwd.letter;
+    let second_match = passchars[pwd.max as usize - 1] == pwd.letter;
+    first_match ^ second_match
 }
