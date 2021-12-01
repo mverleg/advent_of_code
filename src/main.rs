c0ffee6617f5a4fa10e8558b56324c43a0f6f359
Mark <mverleg.noreply@gmail.com>
2021-12-02 00:23:26 +0100

    I think this was the original solution for a before I overwrote it

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,6 @@ mod yr2021;
 use yr2021::*;
 
 fn main() {
-    yr2020::dec06::dec06a()
+    yr2020::dec06::dec06a();
+    yr2020::dec06::dec06b();
 }
--- src/yr2020/dec06.rs
+++ src/yr2020/dec06.rs
@@ -3,29 +3,39 @@ use ::std::collections::HashMap;
 use std::collections::HashSet;
 
 pub fn dec06a() {
-    let res = run();
+    let res = run(unique_answers);
     println!("{}", res);
 }
 
 pub fn dec06b() {
-    let res = run();
+    let res = run(universal_answers);
     println!("{}", res);
 }
 
-fn run() -> u64 {
+fn run(group_counter: fn(&[&str]) -> u64) -> u64 {
     read_to_string("data/2020/dec06.txt")
         .unwrap()
         .lines()
         .collect::<Vec<_>>()
         .split(|ln| ln.trim().is_empty())
-        .map(|grp| {
-            grp.iter()
-                .map(|ans| ans.chars().collect::<HashSet<char>>())
-                .reduce(|a, b| a.intersection(&b)
-                    .cloned()
-                    .collect::<HashSet<char>>() )
-                .unwrap()
-                .len() as u64
-        })
+        .map(group_counter)
         .sum()
+}
+
+fn unique_answers(group: &[&str]) -> u64 {
+    let mut uniq = HashSet::new();
+    group.iter()
+        .flat_map(|ans| ans.chars())
+        .for_each(|ans| {uniq.insert(ans);} );
+    uniq.len() as u64
+}
+
+fn universal_answers(group: &[&str]) -> u64 {
+    group.iter()
+        .map(|ans| ans.chars().collect::<HashSet<char>>())
+        .reduce(|a, b| a.intersection(&b)
+            .cloned()
+            .collect::<HashSet<char>>())
+        .unwrap()
+        .len() as u64
 }
