c0ffee0dcf12c613c836d3e7535c34a32d4d7d4b
Mark <mverleg.noreply@gmail.com>
2021-12-01 22:40:49 +0100

    Demo test with double list, not working yet

--- src/parse.rs
+++ src/parse.rs
@@ -3,3 +3,11 @@ use ::lalrpop_util::lalrpop_mod;
 lalrpop_mod!(pub grammar);
 
 pub use grammar::*;
+
+#[test]
+fn test_real_exprs() {
+    use crate::parse::grammar::RealExprsParser;
+    let reals = RealExprsParser::new()
+        .parse("1.2 3 4.5 6 7")
+        .unwrap();
+}
