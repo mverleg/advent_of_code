c0ffeec6649dceb0ec98803d19b41bd42c2ef545
Mark <mverleg.noreply@gmail.com>
2021-12-06 12:32:09 +0100

    First solution, too low result for example

--- src/yr2021/dec05.rs
+++ src/yr2021/dec05.rs
@@ -1,5 +1,6 @@
 use ::std::collections::HashMap;
 use ::std::fs::read_to_string;
+use std::cmp::{max, min};
 
 use ::itertools::Itertools;
 use ::lazy_static::lazy_static;
@@ -27,28 +28,59 @@ pub fn part_b() {
 
 #[derive(Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
 struct Res {
-    x1: u32,
-    y1: u32,
-    x2: u32,
-    y2: u32,
+    x1: usize,
+    y1: usize,
+    x2: usize,
+    y2: usize,
 }
 
 fn run() -> u64 {
-    // Find id with highest total price
+    let lines = get_lines("data/2021/dec05.txt").into_iter()
+        .map(|line| parse_line(&line))
+        .collect::<Vec<_>>();
 
-    get_lines("data/2021/dec05.txt").into_iter()
-        .map(|line| parse_line(&line));
+    let grid = build_grid(lines);
 
-    unimplemented!()
+    let mut count = 0;
+    for row in grid {
+        for cell in row {
+            if cell > 1 {
+                count += 1
+            }
+        }
+    }
+
+    count as u64
+}
+
+fn build_grid(lines: Vec<Res>) -> Vec<Vec<u8>> {
+    let x_max = lines.iter().map(|res| max(res.x1, res.x2)).max().unwrap() + 1;
+    let y_max = lines.iter().map(|res| max(res.y1, res.y2)).max().unwrap() + 1;
+    let mut grid = vec![vec![0u8; y_max]; x_max];
+    for line in lines {
+        if line.x1 == line.x2 {
+            for y in min(line.y1, line.y2)..max(line.y2, line.y2) {
+                grid[line.x1][y] += 1
+            }
+        } else if line.y1 == line.y2 {
+            for x in min(line.x1, line.x2)..max(line.x1, line.x2) {
+                grid[x][line.y1] += 1
+            }
+        } else {
+            // Skip for part a
+            continue
+        }
+    }
+    grid
 }
 
 fn parse_line(line: &str) -> Res {
     let mut parser = map(tuple((digit1, tag(","), digit1, tag(" -> "), digit1, tag(","), digit1)),
         |(x1, _, y1, _, x2, _, y2): (&str, &str, &str, &str, &str, &str, &str)| Res {
-            x1: x1.parse::<u32>().unwrap(),
-            y1: y1.parse::<u32>().unwrap(),
-            x2: x2.parse::<u32>().unwrap(),
-            y2: y2.parse::<u32>().unwrap()
+            x1: x1.parse::<usize>().unwrap(),
+            y1: y1.parse::<usize>().unwrap(),
+            x2: x2.parse::<usize>().unwrap(),
+            y2: y2.parse::<usize>().unwrap()
         });
     let res: IResult<&str, Res, VerboseError<&str>> = parser(line);
     res.unwrap().1
