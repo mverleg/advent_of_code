c0ffee053001e099c926a7f9852c3048f1b151db
Mark <mverleg.noreply@gmail.com>
2021-12-06 23:05:14 +0100

    Add cache to make it fast

--- src/yr2021/dec06.rs
+++ src/yr2021/dec06.rs
@@ -1,13 +1,9 @@
+use ::std::cell::RefCell;
 use ::std::collections::HashMap;
 use ::std::fs::read_to_string;
+use ::std::sync::RwLock;
 
 use ::itertools::Itertools;
-use ::lazy_static::lazy_static;
-use ::regex::Regex;
-
-lazy_static! {
-    static ref RE: Regex = Regex::new(r"^([0-9]+)\s+([0-9]+)$").unwrap();
-}
 
 const INIT: u64 = 9;
 const CYCLE: u64 = 7;
@@ -18,26 +14,30 @@ pub fn part_a() {
 }
 
 pub fn part_b() {
-    // let res = run(18);
-    // println!("{}", res);
-}
-
-#[derive(Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Clone)]
-struct Res {
-    id: u32,
-    price: u32,
+    let res = run(256);
+    println!("{}", res);
 }
 
 fn run(remaining: u64) -> u64 {
+    let mut cache: HashMap<(u64, u64), u64> = HashMap::new();
     get_lines("data/2021/dec06.txt")[0].split(",")
         .map(|nr| nr.parse::<u64>().unwrap())
-        .map(|nr| end_count(nr, remaining, "init"))
+        .map(|nr| fam_tree_cached(&mut cache, nr, remaining, "init"))
         .sum()
 }
 
-fn end_count(day: u64, remaining: u64, msg: &str) -> u64 {
-    let msg = format!("{} -> {} / {}", msg, day, remaining);
-    eprintln!("{}", msg);
+fn fam_tree_cached(cache: &mut HashMap<(u64, u64), u64>, day: u64, remaining: u64, msg: &str) -> u64 {
+    if let Some(count) = cache.get(&(day, remaining)) {
+        return *count
+    }
+    let count = count_fam_tree(cache, day, remaining, msg);
+    cache.insert((day, remaining), count);
+    count
+}
+
+fn count_fam_tree(cache: &mut HashMap<(u64, u64), u64>, day: u64, remaining: u64, msg: &str) -> u64 {
+    // let msg = format!("{} -> {} / {}", msg, day, remaining);
+    // eprintln!("{}", msg);
     if day > remaining {
         return if remaining > 0 {
             1
@@ -51,7 +51,7 @@ fn end_count(day: u64, remaining: u64, msg: &str) -> u64 {
     let mut spawn_at = (remaining - day) % CYCLE;
     let mut sum = 1;  // current
     while spawn_at + day <= remaining {
-        sum += end_count(INIT, spawn_at, &msg);
+        sum += fam_tree_cached(cache, INIT, spawn_at, &msg);
         spawn_at += CYCLE;
     }
     sum
@@ -59,60 +59,60 @@ fn end_count(day: u64, remaining: u64, msg: &str) -> u64 {
 
 #[test]
 fn test1() {
-    assert_eq!(end_count(5, 3, "test"), 1);
+    assert_eq!(count_fam_tree(, 5, 3, "test"), 1);
 }
 
 #[test]
 fn test2() {
-    assert_eq!(end_count(1, 5, "test"), 2);
+    assert_eq!(count_fam_tree(, 1, 5, "test"), 2);
 }
 
 #[test]
 fn test3() {
-    assert_eq!(end_count(1, 11, "test"), 4);
+    assert_eq!(count_fam_tree(, 1, 11, "test"), 4);
 }
 
 #[test]
 fn test6() {
-    assert_eq!(end_count(0, 1, "test"), 2);
+    assert_eq!(count_fam_tree(, 0, 1, "test"), 2);
 }
 
 #[test]
 fn test7() {
     // new spawns only count next round (when current fish resets back to 6)
-    assert_eq!(end_count(1, 1, "test"), 1);
+    assert_eq!(count_fam_tree(, 1, 1, "test"), 1);
 }
 
 #[test]
 fn test8() {
     // new spawns only count next round (when current fish resets back to 6)
-    assert_eq!(end_count(0, 0, "test"), 1);
+    assert_eq!(count_fam_tree(, 0, 0, "test"), 1);
 }
 
 #[test]
 fn test9() {
     //TODO: verify answer
-    assert_eq!(end_count(0, 46, "test"), 71);
+    assert_eq!(count_fam_tree(, 0, 46, "test"), 71);
 }
 
 #[test]
 fn test_pzzl1() {
-    assert_eq!(end_count(1, 18, "test"), 7);
+    assert_eq!(count_fam_tree(, 1, 18, "test"), 7);
 }
 
 #[test]
 fn test_pzzl2() {
-    assert_eq!(end_count(2, 18, "test"), 5);
+    assert_eq!(count_fam_tree(, 2, 18, "test"), 5);
 }
 
 #[test]
 fn test_pzzl3() {
-    assert_eq!(end_count(3, 18, "test"), 5);
+    assert_eq!(count_fam_tree(, 3, 18, "test"), 5);
 }
 
 #[test]
 fn test_pzzl4() {
-    assert_eq!(end_count(4, 18, "test"), 4);
+    assert_eq!(count_fam_tree(, 4, 18, "test"), 4);
 }
 
 fn get_lines(pth: &str) -> Vec<String> {
