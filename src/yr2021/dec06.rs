c0ffeeb1b264511cb1a10cd2d660e8f9c46406da
Mark <mverleg.noreply@gmail.com>
2021-12-06 18:51:06 +0100

    Potential efficient way to count future spawn

--- src/yr2021/dec06.rs
+++ src/yr2021/dec06.rs
@@ -9,13 +9,16 @@ lazy_static! {
     static ref RE: Regex = Regex::new(r"^([0-9]+)\s+([0-9]+)$").unwrap();
 }
 
+const INIT: u64 = 6;
+const CYCLE: u64 = 8;
+
 pub fn part_a() {
-    let res = run();
+    let res = run(18);
     println!("{}", res);
 }
 
 pub fn part_b() {
-    let res = run();
+    let res = run(18);
     println!("{}", res);
 }
 
@@ -25,30 +28,45 @@ struct Res {
     price: u32,
 }
 
-fn run() -> u64 {
-    // Find id with highest total price
-    get_lines("data/2021/dec06.txt").into_iter()
-        .map(|line| {
-            let groups = RE.captures(&line).unwrap();
-            Res {
-                id: groups[1].parse().unwrap(),
-                price: groups[2].parse().unwrap(),
-            }
-        })
-        .into_group_map_by(|res| res.id)
-        .into_iter()
-        .map(|(k, v)| Res { id: k, price: v.iter().map(|res| res.price).sum() })
-        .inspect(|res| println!("item {} total price {}", res.id, res.price))
-        .sorted_by_key(|res| res.price)
-        .rev()
-        .find(|res| true)
-        .unwrap()
-        .id as u64
+fn run(remaining: u64) -> u64 {
+    get_lines("data/2021/dec06.txt")[0].split(",")
+        .map(|nr| nr.parse::<u64>().unwrap())
+        .map(|nr| end_count(nr, remaining))
+        .sum()
+
 }
+
+fn end_count(day: u64, remaining: u64) -> u64 {
+    if day > remaining {
+        return 1
+    }
+    let mut i = remaining - day;
+    let mut sum = 1;  // self
+    eprintln!("i={} day={} rem={} sum={}", i, day, remaining, sum);
+    loop {
+        eprintln!("spawn: {} / {}", i, remaining);
+        sum += end_count(CYCLE, i);
+        if i > INIT {
+            i -= INIT;
+        } else {
+            break
+        }
+    }
+    sum
+}
+
+#[test]
+fn end_count_test() {
+    assert_eq!(end_count(5, 3), 1);
+    assert_eq!(end_count(1, 5), 2);
+    assert_eq!(end_count(1, 11), 4);
+    assert_eq!(end_count(3, 18), 7);
+}
+
 
 fn get_lines(pth: &str) -> Vec<String> {
     let content = read_to_string(pth).unwrap();
-    contentg
+    content
         .lines()
         .filter(|ln| !ln.trim().is_empty())
         .map(|ln| ln.to_owned())
