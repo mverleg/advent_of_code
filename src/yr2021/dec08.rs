c0ffee7c353da8dd6cca126feace23efddec197a
Mark <mverleg.noreply@gmail.com>
2021-12-10 23:22:53 +0100

    Layout of my, no doubt over-complicated, solution approach

--- src/yr2021/dec08.rs
+++ src/yr2021/dec08.rs
@@ -26,12 +26,32 @@ fn run1() -> u64 {
 
 fn run2() -> u64 {
     parse_input().iter()
-        .map(|(_, outp)| outp)
-        .map(|outp| outp.iter()
-            .map(|word| word.len())
-            .filter(|len| len <= &4 || len == &7)
-            .count())
+        .map(|(ptrns, outp)| (find_mapping(ptrns), outp))
+        .map(|(mapping, outps)| outps.iter()
+            .map(|outp| interpret(outp, mapping))
+            .sum::<usize>())
         .sum::<usize>() as u64
+}
+
+fn find_mapping(ptrns: &Vec<HashSet<char>>) -> [char; 9] {
+    ptrns.iter()
+        .map(|ptrn| possibilities(ptrn))
+        .inspect(|possib| eprintln!("{:?}", possib))
+        .reduce(|a, b| unimplemented!());
+
+    unimplemented!()
+}
+
+fn possibilities(word: &HashSet<char>) -> Vec<(i32, [Option<char>; 9])> {
+    unimplemented!()
+}
+
+fn char2pos(c: char) -> usize {
+    (c as usize) - ('a' as usize)
+}
+
+fn interpret(outp: &HashSet<char>, mapping: [char; 9]) -> usize {
+    unimplemented!()
 }
 
 fn parse_input() -> Vec<(Vec<HashSet<char>>, Vec<HashSet<char>>)> {
