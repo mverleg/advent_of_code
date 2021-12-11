c0ffee2a287a6584bba82370c11926148985b663
Mark <mverleg.noreply@gmail.com>
2021-12-11 12:03:04 +0100

    Going to try new approach

--- src/yr2021/dec08.rs
+++ src/yr2021/dec08.rs
@@ -1,6 +1,5 @@
 use ::std::collections::HashMap;
 use ::std::fs::read_to_string;
-use ::std::collections::HashSet;
 
 use ::itertools::Itertools;
 
@@ -33,7 +32,7 @@ fn run2() -> u64 {
         .sum::<usize>() as u64
 }
 
-fn find_mapping(ptrns: &Vec<HashSet<char>>) -> [char; 9] {
+fn find_mapping(ptrns: &Vec<Vec<char>>) -> [char; 9] {
     ptrns.iter()
         .map(|ptrn| possibilities(ptrn))
         .inspect(|possib| eprintln!("{:?}", possib))
@@ -42,19 +41,34 @@ fn find_mapping(ptrns: &Vec<HashSet<char>>) -> [char; 9] {
     unimplemented!()
 }
 
-fn possibilities(word: &HashSet<char>) -> Vec<(i32, [Option<char>; 9])> {
-    unimplemented!()
+fn possibilities(word: &Vec<char>) -> Vec<(i32, [Option<char>; 9])> {
+    match word.len() {
+        2 => vec![
+            (1, [None, None, Some(word[0]), None, None, None, None, Some(word[1]), None,]),
+            (1, [None, None, Some(word[1]), None, None, None, None, Some(word[0]), None,]),
+        ],
+        3 => unimplemented!(),
+        4 => unimplemented!(),
+        5 => unimplemented!(),
+        6 => unimplemented!(),
+        7 => vec![
+            (7, [Some(word[0]), None, Some(word[1]), None, None, None, None, Some(word[2]), None,]),
+            (7, [Some(word[0]), None, Some(word[2]), None, None, None, None, Some(word[1]), None,]),
+            //TODO @mark: more
+        ],
+        _ => unreachable!(),
+    }
 }
 
 fn char2pos(c: char) -> usize {
     (c as usize) - ('a' as usize)
 }
 
-fn interpret(outp: &HashSet<char>, mapping: [char; 9]) -> usize {
+fn interpret(outp: &Vec<char>, mapping: [char; 9]) -> usize {
     unimplemented!()
 }
 
-fn parse_input() -> Vec<(Vec<HashSet<char>>, Vec<HashSet<char>>)> {
+fn parse_input() -> Vec<(Vec<Vec<char>>, Vec<Vec<char>>)> {
     let inps = get_lines("data/2021/dec08.txt").iter()
         .map(|line| line.split_once(" | ").unwrap())
         .map(|(ptrns, outp)| (space_sep_ints(ptrns), space_sep_ints(outp)))
@@ -62,9 +76,9 @@ fn parse_input() -> Vec<(Vec<HashSet<char>>, Vec<HashSet<char>>)> {
     inps
 }
 
-fn space_sep_ints(txt: &str) -> Vec<HashSet<char>> {
+fn space_sep_ints(txt: &str) -> Vec<Vec<char>> {
     txt.split(" ")
-        .map(|word| word.chars().collect::<HashSet<_>>())
+        .map(|word| word.chars().collect::<Vec<_>>())
         .collect()
 }
 
