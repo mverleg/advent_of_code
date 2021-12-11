c0ffee0197300cb3a270f0ec0798e6c52994c075
Mark <mverleg.noreply@gmail.com>
2021-12-11 13:06:45 +0100

    Use mapping matrices

--- src/yr2021/dec08.rs
+++ src/yr2021/dec08.rs
@@ -25,60 +25,107 @@ fn run1() -> u64 {
 
 fn run2() -> u64 {
     parse_input().iter()
-        .map(|(ptrns, outp)| (find_mapping(ptrns), outp))
-        .map(|(mapping, outps)| outps.iter()
-            .map(|outp| interpret(outp, mapping))
-            .sum::<usize>())
-        .sum::<usize>() as u64
+        .map(|(ptrns, outp)| row(ptrns, outp))
+        .sum()
 }
 
-fn find_mapping(ptrns: &Vec<Vec<char>>) -> [char; 9] {
-    ptrns.iter()
-        .map(|ptrn| possibilities(ptrn))
-        .inspect(|possib| eprintln!("{:?}", possib))
-        .reduce(|a, b| unimplemented!());
+fn row(ptrns: &[Vec<usize>], outp: &[Vec<usize>]) -> u64 {
+    let mapping = find_mapping(ptrns);
+    apply_mapping(mapping, outp)
+}
 
+#[derive(Debug)]
+struct Possible {
+    grid: [[bool; 7]; 7]
+}
+
+impl Possible {
+    fn new() -> Self {
+        Possible {
+            grid: [[true; 7]; 7],
+        }
+    }
+
+    fn disable(&mut self, from: usize, to: usize) -> Self {
+        unimplemented!()
+    }
+
+    fn and(&mut self, other: Self) -> Self {
+        unimplemented!()
+    }
+
+    fn or(&mut self, other: Self) -> Self {
+        unimplemented!()
+    }
+}
+
+fn find_mapping(ptrns: &[Vec<usize>]) -> Possible {
+    let mut current = Possible::new();
+    for ptrn in ptrns {
+        current = current.and(match ptrn.len() {
+            2 => {
+                return Possible::new()
+            },
+            3 => unimplemented!(),
+            4 => unimplemented!(),
+            5 => unimplemented!(),
+            6 => unimplemented!(),
+            7 => unimplemented!(),
+            _ => unreachable!(),
+        })
+    }
+    current
+}
+
+fn apply_mapping(mapping: Possible, outp: &[Vec<usize>]) -> u64 {
     unimplemented!()
 }
 
-fn possibilities(word: &Vec<char>) -> Vec<(i32, [Option<char>; 9])> {
-    match word.len() {
-        2 => vec![
-            (1, [None, None, Some(word[0]), None, None, None, None, Some(word[1]), None,]),
-            (1, [None, None, Some(word[1]), None, None, None, None, Some(word[0]), None,]),
-        ],
-        3 => unimplemented!(),
-        4 => unimplemented!(),
-        5 => unimplemented!(),
-        6 => unimplemented!(),
-        7 => vec![
-            (7, [Some(word[0]), None, Some(word[1]), None, None, None, None, Some(word[2]), None,]),
-            (7, [Some(word[0]), None, Some(word[2]), None, None, None, None, Some(word[1]), None,]),
-            //TODO @mark: more
-        ],
-        _ => unreachable!(),
-    }
-}
+// fn find_mapping(ptrns: &Vec<Vec<usize>>) -> [usize; 9] {
+//     ptrns.iter()
+//         .map(|ptrn| possibilities(ptrn))
+//         .inspect(|possib| eprintln!("{:?}", possib))
+//         .reduce(|a, b| unimplemented!());
+//
+//     unimplemented!()
+// }
+//
+// fn possibilities(word: &Vec<usize>) -> Vec<(i32, [Option<usize>; 9])> {
+//     match word.len() {
+//         2 => vec![
+//             (1, [None, None, Some(word[0]), None, None, None, None, Some(word[1]), None,]),
+//             (1, [None, None, Some(word[1]), None, None, None, None, Some(word[0]), None,]),
+//         ],
+//         3 => unimplemented!(),
+//         4 => unimplemented!(),
+//         5 => unimplemented!(),
+//         6 => unimplemented!(),
+//         7 => vec![
+//             (7, [Some(word[0]), None, Some(word[1]), None, None, None, None, Some(word[2]), None,]),
+//             (7, [Some(word[0]), None, Some(word[2]), None, None, None, None, Some(word[1]), None,]),
+//             //TODO @mark: more
+//         ],
+//         _ => unreachable!(),
+//     }
+// }
 
 fn char2pos(c: char) -> usize {
     (c as usize) - ('a' as usize)
 }
 
-fn interpret(outp: &Vec<char>, mapping: [char; 9]) -> usize {
-    unimplemented!()
-}
-
-fn parse_input() -> Vec<(Vec<Vec<char>>, Vec<Vec<char>>)> {
+fn parse_input() -> Vec<(Vec<Vec<usize>>, Vec<Vec<usize>>)> {
     let inps = get_lines("data/2021/dec08.txt").iter()
         .map(|line| line.split_once(" | ").unwrap())
-        .map(|(ptrns, outp)| (space_sep_ints(ptrns), space_sep_ints(outp)))
+        .map(|(ptrns, outp)| (space_sep_chars(ptrns), space_sep_chars(outp)))
         .collect::<Vec<_>>();
     inps
 }
 
-fn space_sep_ints(txt: &str) -> Vec<Vec<char>> {
+fn space_sep_chars(txt: &str) -> Vec<Vec<usize>> {
     txt.split(" ")
-        .map(|word| word.chars().collect::<Vec<_>>())
+        .map(|word| word.chars()
+            .map(char2pos)
+            .collect::<Vec<_>>())
         .collect()
 }
 
