c0ffee4c97f53f0f3ffcd067b7a94a9aa6d41860
Mark <mverleg.noreply@gmail.com>
2021-12-12 13:42:57 +0100

    Working solution example input

--- src/yr2021/dec09.rs
+++ src/yr2021/dec09.rs
@@ -16,8 +16,18 @@ pub fn part_b() {
 }
 
 fn run() -> u64 {
-    let grid = get_grid("data/2021/dec10.txt");
-
+    let grid = get_grid("data/2021/dec09.txt");
+    let mut minimum_scores = 0;
+    for (x, row) in grid.iter().enumerate() {
+        for (y, val) in row.iter().enumerate() {
+            let is_hor_min = (y == 0 || val < &row[y - 1]) && (y == row.len() - 1 || val < &row[y + 1]);
+            let is_ver_min = (x == 0 || val < &grid[x - 1][y]) && (x == grid.len() - 1 || val < &grid[x + 1][y]);
+            if is_hor_min && is_ver_min {
+                minimum_scores += 1 + (*val as u64)
+            }
+        }
+    }
+    minimum_scores
 }
 
 fn get_grid(pth: &str) -> Vec<Vec<u8>> {
@@ -26,7 +36,7 @@ fn get_grid(pth: &str) -> Vec<Vec<u8>> {
         .lines()
         .filter(|ln| !ln.trim().is_empty())
         .map(|ln| ln.chars()
-            .map(|c| (c - '0') as u8)
+            .map(|c| (c as u8) - ('0' as u8))
             .collect::<Vec<_>>())
         .collect::<Vec<_>>()
 }
