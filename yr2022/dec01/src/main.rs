30e425c63e1ea428193d76532fb2993bfef221c5
Mark <mverleg.noreply@gmail.com>
2022-12-01 08:19:20 +0100

    dec1 slow

--- yr2022/dec01/src/main.rs
+++ yr2022/dec01/src/main.rs
@@ -1,19 +1,36 @@
 use ::std::fs::read_to_string;
 
+// 6:44
+
 fn main() {
     let data = read_to_string("data.txt").unwrap();
-    eprintln!("{}", part_a(&data));
-    //eprintln!("{}", part_b(&data));
+    println!("{}", part_a(&data));
+    eprintln!("{}", part_b(&data));
 }
 
 fn part_a(data: &str) -> usize {
-    run(data)
+    run(data, false)
 }
 
 fn part_b(data: &str) -> usize {
-    run(data)
+    run(data, true)
 }
 
-fn run(data: &str) -> usize {
-    todo!()
+fn run(data: &str, top3: bool) -> usize {
+    let mut elves: Vec<usize> = vec![];
+    let mut current = vec![];
+    for line in data.lines() {
+        if line.is_empty() && !current.is_empty() {
+            elves.push(current.iter().sum());
+            current.clear()
+        } else {
+            current.push(line.parse::<usize>().unwrap());
+        }
+    }
+    elves.sort();
+    if top3 {
+        elves.pop().unwrap() + elves.pop().unwrap() + elves.pop().unwrap()
+    } else {
+        elves.pop().unwrap()
+    }
 }
