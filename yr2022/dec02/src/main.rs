b2787a794dc514d24dc01499d3c969aca2f041fa
Mark <mverleg.noreply@gmail.com>
2022-12-02 08:39:25 +0100

    Working example

--- yr2022/dec02/src/main.rs
+++ yr2022/dec02/src/main.rs
@@ -16,6 +16,19 @@ fn part_b(data: &str) -> usize {
     run(data, true)
 }
 
-fn run(data: &str, top3: bool) -> usize {
-    todo!()
+fn run(data: &str, is_b: bool) -> usize {
+    data.lines()
+        .map(|line| match line {
+            "A X" => 1 + 3,
+            "A Y" => 2 + 6,
+            "A Z" => 3 + 0,
+            "B X" => 1 + 0,
+            "B Y" => 2 + 3,
+            "B Z" => 3 + 6,
+            "C X" => 1 + 6,
+            "C Y" => 2 + 0,
+            "C Z" => 3 + 3,
+            "" => 0,
+            _ => todo!(),
+        }).sum()
 }
