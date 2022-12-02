283dfd95b0de206994ed11e53bda7a63b2ef6c45
Mark <mverleg.noreply@gmail.com>
2022-12-02 08:32:49 +0100

    Create template for dec 2

---
+++ yr2022/dec02/src/main.rs
@@ -0,0 +1,21 @@
+use ::std::fs::read_to_string;
+
+// 6:44
+
+fn main() {
+    let data = read_to_string("data.txt").unwrap();
+    println!("{}", part_a(&data));
+    eprintln!("{}", part_b(&data));
+}
+
+fn part_a(data: &str) -> usize {
+    run(data, false)
+}
+
+fn part_b(data: &str) -> usize {
+    run(data, true)
+}
+
+fn run(data: &str, top3: bool) -> usize {
+    todo!()
+}
