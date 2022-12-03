929fce063d71b67f41e8f6a55ed8dffd71451038
Mark <mverleg.noreply@gmail.com>
2022-12-03 11:54:24 +0100

    part a for dec 3

--- yr2022/dec03/src/main.rs
+++ yr2022/dec03/src/main.rs
@@ -1,6 +1,9 @@
-use ::std::fs::read_to_string;
+#![feature(exclusive_range_pattern)]
 
-// 6:44
+use ::std::fs::read_to_string;
+use std::collections::HashSet;
+
+//
 
 fn main() {
     let data = read_to_string("data.txt").unwrap();
@@ -17,5 +20,26 @@ fn part_b(data: &str) -> usize {
 }
 
 fn run(data: &str, is_b: bool) -> usize {
-    todo!()
+    let mut prio = 0;
+    for line in data.lines() {
+        let (left, right) = line.split_at(line.len() / 2);
+        let left = left.chars().collect::<HashSet<char>>();
+        let right = right.chars().collect::<HashSet<char>>();
+        for item in left {
+            if right.contains(&item) {
+                let p = match item {
+                    'a'..='z' => to_nr(item) - to_nr('a') + 1,
+                    'A'..='Z' => to_nr(item) - to_nr('A') + 26 + 1,
+                    l => todo!("invalid: {l}"),
+                };
+                //eprintln!("{p}");
+                prio += p
+            }
+        }
+    }
+    prio
+}
+
+fn to_nr(c: char) -> usize {
+    <char as Into<u32>>::into(c) as usize
 }
