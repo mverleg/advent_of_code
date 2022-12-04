cf86fca8bd4e373413b2497ce702f2f17e619e94
Mark <mverleg.noreply@gmail.com>
2022-12-04 11:03:50 +0100

    First draft

--- yr2022/dec04/src/main.rs
+++ yr2022/dec04/src/main.rs
@@ -8,6 +8,26 @@ fn main() {
     println!("{}", part_b(&data));
 }
 
+#[derive(Debug)]
+struct Section {
+    start: usize,
+    end: usize,
+}
+
+impl Section {
+    fn parse(text: &str) -> Self {
+        let (start, end) = text.split_once(",").unwrap();
+        Section {
+            start: start.parse().unwrap(),
+            end: end.parse().unwrap(),
+        }
+    }
+
+    fn contains(&self, other: &Section) -> bool {
+        other.start >= self.start && other.end <= self.end
+    }
+}
+
 fn part_a(data: &str) -> usize {
     run(data, false)
 }
@@ -17,9 +37,12 @@ fn part_b(data: &str) -> usize {
 }
 
 fn run(data: &str, is_b: bool) -> usize {
-    let mut result = vec![];
+    let mut count = 0;
     for line in data.lines() {
-
+        let (left, right) = line.split_once(",").unwrap();
+        if left.contains(right) || right.contains(left) {
+            count += 1;
+        }
     }
-    result.sum()
+    count
 }
