c07dc6aa59c3fcea7c3be8d74db7d5e47420ea6d
Mark <mverleg.noreply@gmail.com>
2022-12-07 07:52:15 +0100

    Size for dirname, without nesting

--- yr2022/dec07/src/main.rs
+++ yr2022/dec07/src/main.rs
@@ -1,4 +1,5 @@
 use ::std::fs::read_to_string;
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 
 //
@@ -22,16 +23,17 @@ fn run(data: &str, is_b: bool) -> usize {
     let mut dir_total_sizes: HashMap<&str, usize> = HashMap::new();
     let mut current_path = vec!["/"];
     for line in data.lines() {
-        eprintln!("> {line}");
+        //eprintln!("> {line}");
         if line.starts_with("$ ") {
             let cmd = &line[2..];
-            eprintln!(">> |{cmd}|");
+            //eprintln!(">> |{cmd}|");
             if cmd == "cd /" {
-                current_path = vec!["/"];
+                current_path.clear();
+                current_path.push("/");
             } else if cmd == "cd .." {
                 current_path.pop();
             } else if cmd.starts_with("cd ") {
-                let (_, dir) = cmd.split_once(" ").unwrap();
+                let (_, dir) = cmd.split_once(' ').unwrap();
                 current_path.push(dir);
             } else if cmd == "ls" {
                 // do nothing
@@ -43,8 +45,18 @@ fn run(data: &str, is_b: bool) -> usize {
                 continue
             }
             assert!(line.chars().next().unwrap().is_digit(10), "not digit: {line}");
-            todo!()
+            let (size, _) = line.split_once(' ').expect("expected space in dir listing entry");
+            let size: usize = size.parse().unwrap();
+            for part in current_path.iter() {
+                let subpath = part;
+                //TODO @mark: ^
+                match dir_total_sizes.entry(subpath) {
+                    Entry::Occupied(mut entry) => *entry.get_mut() += size,
+                    Entry::Vacant(entry) => { entry.insert(size); },
+                }
+            }
         }
     }
+    dbg!(dir_total_sizes);
     todo!()
 }
