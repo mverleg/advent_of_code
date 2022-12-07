82d3ac15ec54d0e161e97f34c43951cc4c2f6d8c
Mark <mverleg.noreply@gmail.com>
2022-12-07 07:56:40 +0100

    solve a for demo

--- yr2022/dec07/src/main.rs
+++ yr2022/dec07/src/main.rs
@@ -20,7 +20,7 @@ fn part_b(data: &str) -> usize {
 }
 
 fn run(data: &str, is_b: bool) -> usize {
-    let mut dir_total_sizes: HashMap<&str, usize> = HashMap::new();
+    let mut dir_total_sizes: HashMap<String, usize> = HashMap::new();
     let mut current_path = vec!["/"];
     for line in data.lines() {
         //eprintln!("> {line}");
@@ -47,9 +47,9 @@ fn run(data: &str, is_b: bool) -> usize {
             assert!(line.chars().next().unwrap().is_digit(10), "not digit: {line}");
             let (size, _) = line.split_once(' ').expect("expected space in dir listing entry");
             let size: usize = size.parse().unwrap();
-            for part in current_path.iter() {
-                let subpath = part;
-                //TODO @mark: ^
+            for i in 0 .. current_path.len() {
+                let mut subpath = "/".to_owned();
+                subpath.push_str(&current_path[1..i+1].join("/"));
                 match dir_total_sizes.entry(subpath) {
                     Entry::Occupied(mut entry) => *entry.get_mut() += size,
                     Entry::Vacant(entry) => { entry.insert(size); },
@@ -57,6 +57,9 @@ fn run(data: &str, is_b: bool) -> usize {
             }
         }
     }
-    dbg!(dir_total_sizes);
-    todo!()
+    dbg!(&dir_total_sizes);
+    dir_total_sizes.into_iter()
+        .map(|(_, size)| size)
+        .filter(|size| *size <= 100000)
+        .sum()
 }
