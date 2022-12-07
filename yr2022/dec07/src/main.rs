c4fc9858b8238065ccdc1a8477889d43aa56cfec
Mark <mverleg.noreply@gmail.com>
2022-12-07 08:00:35 +0100

    Faild attempt at b

--- yr2022/dec07/src/main.rs
+++ yr2022/dec07/src/main.rs
@@ -1,6 +1,7 @@
 use ::std::fs::read_to_string;
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
+use itertools::Itertools;
 
 //
 
@@ -12,14 +13,26 @@ fn main() {
 
 
 fn part_a(data: &str) -> usize {
-    run(data, false)
+    let dir_total_sizes = get_dir_sizes(data);
+    dir_total_sizes.into_iter()
+        .map(|(_, size)| size)
+        .filter(|size| *size <= 100000)
+        .sum()
 }
 
 fn part_b(data: &str) -> usize {
-    run(data, true)
+    let dir_total_sizes = get_dir_sizes(data);
+    let space_use = *dir_total_sizes.get("/").expect("no root");
+    assert!(space_use < 70000000);
+    let space_need = 70000000 - space_use;
+    dir_total_sizes.into_iter()
+        .map(|(_, size)| size)
+        .filter(|size| *size >= space_need)
+        .sorted()
+        .next().expect("no match")
 }
 
-fn run(data: &str, is_b: bool) -> usize {
+fn get_dir_sizes(data: &str) -> HashMap<String, usize> {
     let mut dir_total_sizes: HashMap<String, usize> = HashMap::new();
     let mut current_path = vec!["/"];
     for line in data.lines() {
@@ -47,9 +60,9 @@ fn run(data: &str, is_b: bool) -> usize {
             assert!(line.chars().next().unwrap().is_digit(10), "not digit: {line}");
             let (size, _) = line.split_once(' ').expect("expected space in dir listing entry");
             let size: usize = size.parse().unwrap();
-            for i in 0 .. current_path.len() {
+            for i in 0..current_path.len() {
                 let mut subpath = "/".to_owned();
-                subpath.push_str(&current_path[1..i+1].join("/"));
+                subpath.push_str(&current_path[1..i + 1].join("/"));
                 match dir_total_sizes.entry(subpath) {
                     Entry::Occupied(mut entry) => *entry.get_mut() += size,
                     Entry::Vacant(entry) => { entry.insert(size); },
@@ -57,9 +70,5 @@ fn run(data: &str, is_b: bool) -> usize {
             }
         }
     }
-    dbg!(&dir_total_sizes);
-    dir_total_sizes.into_iter()
-        .map(|(_, size)| size)
-        .filter(|size| *size <= 100000)
-        .sum()
+    dir_total_sizes
 }
