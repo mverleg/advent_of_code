d67dd821739d158627906a3c8a85e1329026d831
Mark <mverleg.noreply@gmail.com>
2022-12-08 21:01:40 +0100

    One viewing direction, marking already-seen trees

--- yr2022/dec08/src/main.rs
+++ yr2022/dec08/src/main.rs
@@ -17,9 +17,31 @@ fn part_b(data: &str) -> usize {
 }
 
 fn run(data: &str, is_b: bool) -> usize {
-    let mut res = 0;
-    for line in data.lines() {
-        todo!()
+    let mut grid = parse_grid(data);
+    let mut vis_cnt = 0;
+    for row in grid.iter_mut() {
+        let mut highest = 0;
+        for col in row.iter_mut() {
+            if *col > highest {
+                highest = *col;
+                vis_cnt += 1;
+                *col = 0;
+            }
+        }
     }
-    res
+    eprintln!("vis_cnt={vis_cnt}");
+    todo!()
+}
+
+fn parse_grid(data: &str) -> Vec<Vec<usize>> {
+    let mut grid = vec![];
+    for line in data.lines() {
+        let mut row = vec![];
+        for chr in line.chars() {
+            row.push(chr.to_string().parse::<usize>().unwrap() + 1)
+        }
+        grid.push(row);
+    }
+    // assumed square
+    grid
 }
