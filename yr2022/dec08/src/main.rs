83f0c856687c2b48f4750fc1b94a32a4fd07a620
Mark <mverleg.noreply@gmail.com>
2022-12-08 21:53:21 +0100

    Remove redundant level of nesting

--- yr2022/dec08/src/main.rs
+++ yr2022/dec08/src/main.rs
@@ -4,19 +4,11 @@ use ::std::fs::read_to_string;
 
 fn main() {
     let data = read_to_string("data.txt").unwrap();
-    println!("{}", part_a(&data));
+    //println!("{}", part_a(&data));
     println!("{}", part_b(&data));
 }
 
 fn part_a(data: &str) -> usize {
-    run(data, false)
-}
-
-fn part_b(data: &str) -> usize {
-    run(data, true)
-}
-
-fn run(data: &str, is_b: bool) -> usize {
     let mut grid = parse_grid(data);
     let mut seen = vec![vec![false; grid[0].len()]; grid.len()];
     let mut vis_cnt = 0;
@@ -42,7 +34,7 @@ fn run(data: &str, is_b: bool) -> usize {
             }
         }
     }
-    for j in 0 .. grid[0].len() {
+    for j in 0..grid[0].len() {
         let mut highest = 0;
         for i in 0..grid.len() {
             let val = &mut grid[i][j];
@@ -77,6 +69,65 @@ fn run(data: &str, is_b: bool) -> usize {
         eprintln!("");
     }
     vis_cnt
+}
+
+fn part_b(data: &str) -> usize {
+    let mut grid = parse_grid(data);
+    let mut highest_scenic = (0, 0, 0);
+    for a in 1..grid.len() - 1 {
+        for b in 1..grid[0].len() - 1 {
+            let mut scenic: usize = 1;
+            let start = grid[a][b];
+
+            let mut vis_cnt = 0;
+            for i in 0..a-1 {
+                let val = &mut grid[i][b];
+                if *val >= start {
+                    break
+                }
+                vis_cnt += 1;
+            }
+            scenic *= vis_cnt;
+
+            let mut vis_cnt = 0;
+            for i in a+1..grid.len() {
+                let val = &mut grid[i][b];
+                if *val >= start {
+                    break
+                }
+                vis_cnt += 1;
+            }
+            scenic *= vis_cnt;
+
+            let mut vis_cnt = 0;
+            for j in 0..b-1 {
+                let val = &mut grid[a][j];
+                if *val >= start {
+                    break
+                }
+                vis_cnt += 1;
+            }
+            scenic *= vis_cnt;
+
+            let mut vis_cnt = 0;
+            for j in b+1..grid[0].len() {
+                let val = &mut grid[a][j];
+                if *val >= start {
+                    break
+                }
+                vis_cnt += 1;
+            }
+            scenic *= vis_cnt;
+
+            if scenic > highest_scenic.0 {
+                eprintln!("update {} -> {scenic}", highest_scenic.0);  //TODO @mark: TEMPORARY! REMOVE THIS!
+                highest_scenic = (scenic, a, b);
+            }
+        }
+    }
+    assert!(highest_scenic.0 >= 0);
+    eprintln!("{} for {},{}", highest_scenic.0, highest_scenic.1, highest_scenic.2);
+    highest_scenic.0 as usize
 }
 
 fn parse_grid(data: &str) -> Vec<Vec<usize>> {
