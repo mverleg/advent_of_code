fdab004295828c102dcd9ec75df3af3590ffb5a1
Mark <mverleg.noreply@gmail.com>
2022-12-09 08:42:46 +0100

    Head movement

--- yr2022/dec09/src/main.rs
+++ yr2022/dec09/src/main.rs
@@ -1,4 +1,5 @@
 use ::std::fs::read_to_string;
+use std::collections::HashSet;
 
 //
 
@@ -18,8 +19,23 @@ fn part_b(data: &str) -> usize {
 
 fn run(data: &str, is_b: bool) -> usize {
     let mut res = 0;
+    let mut head = (0, 0);
+    let mut tail = (0, 0);
+    let mut seen = HashSet::new();
     for line in data.lines() {
-        todo!()
+        let (dir, amt) = line.split_once(' ').expect("cannot parse line");
+        let amt = amt.parse().expect("incorrect step size");
+        for _ in 0..amt {
+            match dir {
+                "R" => head.0 += 1,
+                "U" => head.1 += 1,
+                "L" => head.0 -= 1,
+                "D" => head.1 -= 1,
+                _ => panic!("unknown direction"),
+            }
+            seen.insert(tail);
+        }
     }
+    eprintln!("head final pos: {},{}", head.0, head.1);
     res
 }
