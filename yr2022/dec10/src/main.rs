53ac8f976216abb2b18e425afa35f1ad9fce0d98
Mark <mverleg.noreply@gmail.com>
2022-12-10 11:42:51 +0100

    Rewrite A in full register array style

--- yr2022/dec10/src/main.rs
+++ yr2022/dec10/src/main.rs
@@ -5,78 +5,30 @@ use ::std::fs::read_to_string;
 fn main() {
     let data = read_to_string("data.txt").unwrap();
     println!("{}\n", part_a(&data));
-    println!("{}", part_b(&data));
+    //println!("{}", part_b(&data));
 }
 
 fn part_a(data: &str) -> i64 {
-    let mut signal_strength = 0;
-    let mut cycle: i64 = 0;
-    let mut register = 1;
-    let mut next_recording_cycle = 20;
-    for line in data.lines() {
-        let mut addition: i64 = 0;
-        if line.starts_with("noop") {
-            cycle += 1
-        } else if line.starts_with("addx") {
-            addition = line[5..].parse().expect("cannot parse addx digit");
-            cycle += 2;
-        } else {
-            panic!("unknown line: {line}")
+    let registers = fill_registers(data);
+    for (i, reg) in registers.into_iter().enumerate() {
+        if (i + 22) % 40 <= 2 {
+            eprintln!("{i} {reg}");
         }
-        let extra_cycles = cycle - next_recording_cycle;
-        if extra_cycles >= 0 {
-            let extra_signal_strength = (cycle - extra_cycles) * register;
-            signal_strength += extra_signal_strength;
-            //eprintln!("{cycle} {register} +{extra_signal_strength} = {signal_strength}");
-            next_recording_cycle += 40;
-        }
-        register += addition;
     }
-    signal_strength
+    let mut total: i64 = 0;
+    let mut i = 19;
+    while i < registers.len() {
+        total += (i + 1) as i64 * registers[i];
+        //eprintln!(">> {i} {}: +{} = {}", registers[i], (i + 1) as i64 * registers[i], total);
+        i += 40;
+    }
+    total
 }
 
 fn part_b(data: &str) -> i64 {
-    let mut cycle: usize = 0;
-    let mut registers = [0i64; 240];
-    for line in data.lines() {
-        if line.starts_with("noop") {
-            if cycle+1 < registers.len() {
-                registers[cycle + 1] = registers[cycle];
-            } else {
-                // eprintln!("endA {cycle}");
-            }
-            cycle += 1;
-        } else if line.starts_with("addx") {
-            let addition: i64 = line[5..].parse().expect("cannot parse addx digit");
-            if cycle+1 < registers.len() {
-                registers[cycle + 1] = registers[cycle];
-            } else {
-                // eprintln!("endB {cycle}");
-            }
-            if cycle+2 < registers.len() {
-                registers[cycle + 2] = registers[cycle] + addition;
-            } else {
-                // eprintln!("endC {cycle}");
-            }
-            cycle += 2;
-        } else {
-            panic!("unknown line: {line}")
-        }
-        // for offset in 1..=cycle_step {
-        //     eprint!("{cycle}+{offset} ({}) {register} +{addition}", cycle+offset);
-        //     let is_sprite = cycle+offset >= register - 1 && cycle+offset <= register + 1;
-        //     screen[(cycle + offset - 1) as usize % screen.len()] = is_sprite;
-        //     if is_sprite {
-        //         eprintln!(" DRAW");
-        //     } else {
-        //         eprintln!(" .");
-        //     }
-        // }
-        // cycle += cycle_step;
-        // register += addition;
-    }
+    let registers = fill_registers(data);
     for (i, reg) in registers.into_iter().enumerate() {
-        //eprintln!("{i} {reg}");
+        eprintln!("{i} {reg}");
         let row_i = (i % 40) as i64;
         if row_i == 0 {
             println!();
@@ -88,16 +40,37 @@ fn part_b(data: &str) -> i64 {
             print!(".");
         }
     }
-    // for (i, pixel) in screen.into_iter().enumerate() {
-    //     if i % 40 == 0 {
-    //         println!()
-    //     }
-    //     if pixel {
-    //         print!("#")
-    //     } else {
-    //         print!(".")
-    //     }
-    // }
     println!();
     0
+}
+
+fn fill_registers(data: &str) -> [i64; 240] {
+    let mut cycle: usize = 0;
+    let mut registers = [1i64; 240];
+    for line in data.lines() {
+        if line.starts_with("noop") {
+            if cycle + 1 < registers.len() {
+                registers[cycle + 1] = registers[cycle];
+            } else {
+                // eprintln!("endA {cycle}");
+            }
+            cycle += 1;
+        } else if line.starts_with("addx") {
+            let addition: i64 = line[5..].parse().expect("cannot parse addx digit");
+            if cycle + 1 < registers.len() {
+                registers[cycle + 1] = registers[cycle];
+            } else {
+                // eprintln!("endB {cycle}");
+            }
+            if cycle + 2 < registers.len() {
+                registers[cycle + 2] = registers[cycle] + addition;
+            } else {
+                // eprintln!("endC {cycle}");
+            }
+            cycle += 2;
+        } else {
+            panic!("unknown line: {line}")
+        }
+    }
+    registers
 }
