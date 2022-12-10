b2349f75b84fe35eef37097a04ccdac8d7a7f7aa
Mark <mverleg.noreply@gmail.com>
2022-12-10 11:30:19 +0100

    Keep track of all registers, easier

--- yr2022/dec10/src/main.rs
+++ yr2022/dec10/src/main.rs
@@ -36,44 +36,68 @@ fn part_a(data: &str) -> i64 {
 }
 
 fn part_b(data: &str) -> i64 {
-    let mut screen = [false; 240];
-    let mut cycle: i64 = 0;
-    let mut register = 1;
+    let mut cycle: usize = 0;
+    let mut registers = [0i64; 240];
     for line in data.lines() {
-        let mut addition: i64 = 0;
-        let mut cycle_step;
         if line.starts_with("noop") {
-            cycle_step = 1
+            if cycle+1 < registers.len() {
+                registers[cycle + 1] = registers[cycle];
+            } else {
+                // eprintln!("endA {cycle}");
+            }
+            cycle += 1;
         } else if line.starts_with("addx") {
-            addition = line[5..].parse().expect("cannot parse addx digit");
-            cycle_step = 2;
+            let addition: i64 = line[5..].parse().expect("cannot parse addx digit");
+            if cycle+1 < registers.len() {
+                registers[cycle + 1] = registers[cycle];
+            } else {
+                // eprintln!("endB {cycle}");
+            }
+            if cycle+2 < registers.len() {
+                registers[cycle + 2] = registers[cycle] + addition;
+            } else {
+                // eprintln!("endC {cycle}");
+            }
+            cycle += 2;
         } else {
             panic!("unknown line: {line}")
         }
-
-        for offset in 1..=cycle_step {
-            eprint!("{cycle}+{offset} ({}) {register}", cycle+offset);
-            let is_sprite = cycle+offset >= register - 1 && cycle+offset <= register + 1;
-            screen[(cycle + offset) as usize % screen.len()] = is_sprite;
-            if is_sprite {
-                eprintln!(" DRAW");
-            } else {
-                eprintln!(" .");
-            }
-        }
-        cycle += cycle_step;
-        register += addition;
+        // for offset in 1..=cycle_step {
+        //     eprint!("{cycle}+{offset} ({}) {register} +{addition}", cycle+offset);
+        //     let is_sprite = cycle+offset >= register - 1 && cycle+offset <= register + 1;
+        //     screen[(cycle + offset - 1) as usize % screen.len()] = is_sprite;
+        //     if is_sprite {
+        //         eprintln!(" DRAW");
+        //     } else {
+        //         eprintln!(" .");
+        //     }
+        // }
+        // cycle += cycle_step;
+        // register += addition;
     }
-    for (i, pixel) in screen.into_iter().enumerate() {
-        if i % 40 == 0 {
-            println!()
+    for (i, reg) in registers.into_iter().enumerate() {
+        //eprintln!("{i} {reg}");
+        let row_i = (i % 40) as i64;
+        if row_i == 0 {
+            println!();
         }
-        if pixel {
-            print!("#")
+        let is_pixel_on = row_i == reg;
+        if is_pixel_on {
+            print!("#");
         } else {
-            print!(".")
+            print!(".");
         }
     }
+    // for (i, pixel) in screen.into_iter().enumerate() {
+    //     if i % 40 == 0 {
+    //         println!()
+    //     }
+    //     if pixel {
+    //         print!("#")
+    //     } else {
+    //         print!(".")
+    //     }
+    // }
     println!();
     0
 }
