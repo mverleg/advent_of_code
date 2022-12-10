046391c09cf7dfbc81f86a7da27f80093254c3e8
Mark <mverleg.noreply@gmail.com>
2022-12-10 11:14:44 +0100

    First line seems to draw

--- yr2022/dec10/src/main.rs
+++ yr2022/dec10/src/main.rs
@@ -4,7 +4,7 @@ use ::std::fs::read_to_string;
 
 fn main() {
     let data = read_to_string("data.txt").unwrap();
-    println!("{}", part_a(&data));
+    println!("{}\n", part_a(&data));
     println!("{}", part_b(&data));
 }
 
@@ -37,29 +37,31 @@ fn part_a(data: &str) -> i64 {
 
 fn part_b(data: &str) -> i64 {
     let mut screen = [false; 240];
-    let mut signal_strength = 0;
     let mut cycle: i64 = 0;
     let mut register = 1;
-    let mut next_recording_cycle = 20;
     for line in data.lines() {
         let mut addition: i64 = 0;
+        let mut cycle_step;
         if line.starts_with("noop") {
-            cycle += 1
+            cycle_step = 1
         } else if line.starts_with("addx") {
             addition = line[5..].parse().expect("cannot parse addx digit");
-            cycle += 2;
+            cycle_step = 2;
         } else {
             panic!("unknown line: {line}")
         }
-        let extra_cycles = cycle - next_recording_cycle;
-        if extra_cycles >= 0 {
-            let extra_signal_strength = (cycle - extra_cycles) * register;
-            signal_strength += extra_signal_strength;
-            eprintln!("{cycle} {register} +{extra_signal_strength} = {signal_strength}");
-            next_recording_cycle += 40;
-        } else {
-            //eprintln!("{cycle} {register}");
+
+        for offset in 1..=cycle_step {
+            eprint!("{cycle}+{offset} ({}) {register}", cycle+offset);
+            let is_sprite = cycle+offset >= register - 1 && cycle+offset <= register + 1;
+            screen[(cycle + offset) as usize % screen.len()] = is_sprite;
+            if is_sprite {
+                eprintln!(" DRAW");
+            } else {
+                eprintln!(" .");
+            }
         }
+        cycle += cycle_step;
         register += addition;
     }
     for (i, pixel) in screen.into_iter().enumerate() {
@@ -73,5 +75,5 @@ fn part_b(data: &str) -> i64 {
         }
     }
     println!();
-    signal_strength
+    0
 }
