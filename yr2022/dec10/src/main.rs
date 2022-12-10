799094857b1e6a52005d8dc3a979d96e92d86844
Mark <mverleg.noreply@gmail.com>
2022-12-10 10:56:52 +0100

    Not elegant but works for demo

--- yr2022/dec10/src/main.rs
+++ yr2022/dec10/src/main.rs
@@ -8,29 +8,39 @@ fn main() {
     println!("{}", part_b(&data));
 }
 
-fn part_a(data: &str) -> usize {
+fn part_a(data: &str) -> i64 {
     run(data, false)
 }
 
-fn part_b(data: &str) -> usize {
+fn part_b(data: &str) -> i64 {
     run(data, true)
 }
 
-fn run(data: &str, is_b: bool) -> usize {
+fn run(data: &str, is_b: bool) -> i64 {
     let mut signal_strength = 0;
-    let mut cycle: usize = 0;
+    let mut cycle: i64 = 0;
     let mut register = 1;
+    let mut next_recording_cycle = 20;
     for line in data.lines() {
+        let mut addition: i64 = 0;
         if line.starts_with("noop") {
             cycle += 1
         } else if line.starts_with("addx") {
-            let addition: i64 = line[5..].parse().expect("cannot parse addx digit");
+            addition = line[5..].parse().expect("cannot parse addx digit");
             cycle += 2;
-            register += addition;
         } else {
             panic!("unknown line: {line}")
         }
-        eprintln!("{cycle} {register}");
+        let extra_cycles = cycle - next_recording_cycle;
+        if extra_cycles >= 0 {
+            let extra_signal_strength = (cycle - extra_cycles) * register;
+            signal_strength += extra_signal_strength;
+            eprintln!("{cycle} {register} +{extra_signal_strength} = {signal_strength}");
+            next_recording_cycle += 40;
+        } else {
+            eprintln!("{cycle} {register}");
+        }
+        register += addition;
     }
     signal_strength
 }
