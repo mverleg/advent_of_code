3020fe551a8a5c1955883bd9c40ccfef61e0c0a2
Mark <mverleg.noreply@gmail.com>
2022-12-10 11:03:08 +0100

    Draw (empty) screen

--- yr2022/dec10/src/main.rs
+++ yr2022/dec10/src/main.rs
@@ -9,14 +9,34 @@ fn main() {
 }
 
 fn part_a(data: &str) -> i64 {
-    run(data, false)
+    let mut signal_strength = 0;
+    let mut cycle: i64 = 0;
+    let mut register = 1;
+    let mut next_recording_cycle = 20;
+    for line in data.lines() {
+        let mut addition: i64 = 0;
+        if line.starts_with("noop") {
+            cycle += 1
+        } else if line.starts_with("addx") {
+            addition = line[5..].parse().expect("cannot parse addx digit");
+            cycle += 2;
+        } else {
+            panic!("unknown line: {line}")
+        }
+        let extra_cycles = cycle - next_recording_cycle;
+        if extra_cycles >= 0 {
+            let extra_signal_strength = (cycle - extra_cycles) * register;
+            signal_strength += extra_signal_strength;
+            //eprintln!("{cycle} {register} +{extra_signal_strength} = {signal_strength}");
+            next_recording_cycle += 40;
+        }
+        register += addition;
+    }
+    signal_strength
 }
 
 fn part_b(data: &str) -> i64 {
-    run(data, true)
-}
-
-fn run(data: &str, is_b: bool) -> i64 {
+    let mut screen = [false; 240];
     let mut signal_strength = 0;
     let mut cycle: i64 = 0;
     let mut register = 1;
@@ -38,9 +58,20 @@ fn run(data: &str, is_b: bool) -> i64 {
             eprintln!("{cycle} {register} +{extra_signal_strength} = {signal_strength}");
             next_recording_cycle += 40;
         } else {
-            eprintln!("{cycle} {register}");
+            //eprintln!("{cycle} {register}");
         }
         register += addition;
     }
+    for (i, pixel) in screen.into_iter().enumerate() {
+        if i % 40 == 0 {
+            println!()
+        }
+        if pixel {
+            print!("#")
+        } else {
+            print!(".")
+        }
+    }
+    println!();
     signal_strength
 }
--- yr2022/template/src/main.rs
+++ yr2022/template/src/main.rs
@@ -4,8 +4,8 @@ use ::std::fs::read_to_string;
 
 fn main() {
     let data = read_to_string("data.txt").unwrap();
-    println!("{}", part_a(&data));
-    println!("{}", part_b(&data));
+    println!("A: {}", part_a(&data));
+    println!("B: {}", part_b(&data));
 }
 
 fn part_a(data: &str) -> usize {
