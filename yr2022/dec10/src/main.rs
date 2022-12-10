9c68e02a0294fefb790451c3cc0bf90fb31afeed
Mark <mverleg.noreply@gmail.com>
2022-12-10 10:47:30 +0100

    Simple simulation, without signal strength so far

--- yr2022/dec10/src/main.rs
+++ yr2022/dec10/src/main.rs
@@ -17,9 +17,20 @@ fn part_b(data: &str) -> usize {
 }
 
 fn run(data: &str, is_b: bool) -> usize {
-    let mut res = 0;
+    let mut signal_strength = 0;
+    let mut cycle: usize = 0;
+    let mut register = 1;
     for line in data.lines() {
-        todo!()
+        if line.starts_with("noop") {
+            cycle += 1
+        } else if line.starts_with("addx") {
+            let addition: i64 = line[5..].parse().expect("cannot parse addx digit");
+            cycle += 2;
+            register += addition;
+        } else {
+            panic!("unknown line: {line}")
+        }
+        eprintln!("{cycle} {register}");
     }
-    res
+    signal_strength
 }
