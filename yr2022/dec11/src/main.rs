ccaf06e80bed305a32e12b75c63805884be70898
Mark <mverleg.noreply@gmail.com>
2022-12-11 17:00:35 +0100

    Init

---
+++ yr2022/dec11/src/main.rs
@@ -0,0 +1,25 @@
+use ::std::fs::read_to_string;
+
+//
+
+fn main() {
+    let data = read_to_string("data.txt").unwrap();
+    println!("A: {}", part_a(&data));
+    println!("B: {}", part_b(&data));
+}
+
+fn part_a(data: &str) -> usize {
+    run(data, false)
+}
+
+fn part_b(data: &str) -> usize {
+    run(data, true)
+}
+
+fn run(data: &str, is_b: bool) -> usize {
+    let mut res = 0;
+    for line in data.lines() {
+        todo!()
+    }
+    res
+}
