ef5fd4384495fadc11074950b42a7856dafcf354
Mark <mverleg.noreply@gmail.com>
2022-12-12 12:05:09 +0100

    Wrapper class

--- yr2022/dec12/src/main.rs
+++ yr2022/dec12/src/main.rs
@@ -8,6 +8,12 @@ fn main() {
     println!("B: {}", part_b(&data));
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Pos {
+    x: usize,
+    y: usize,
+}
+
 fn part_a(data: &str) -> usize {
     run(data, false)
 }
@@ -22,18 +28,18 @@ fn run(data: &str, is_b: bool) -> usize {
     todo!();
 }
 
-fn parse(data: &str) -> ((usize, usize), (usize, usize), Vec<Vec<u8>>) {
-    let mut start = (0, 0);
-    let mut end = (0, 0);
+fn parse(data: &str) -> (Pos, Pos, Vec<Vec<u8>>) {
+    let mut start = Pos { x: usize::MAX, y: usize::MAX };
+    let mut end = Pos { x: usize::MAX, y: usize::MAX };
     let mut grid = vec![];
-    for (i, line) in data.lines().enumerate() {
+    for (x, line) in data.lines().enumerate() {
         let mut row = vec![];
-        for (j, chr) in line.chars().enumerate() {
+        for (y, chr) in line.chars().enumerate() {
             let val = if chr == 'S' {
-                start = (i, j);
+                start = Pos { x, y };
                 0
             } else if chr == 'E' {
-                end = (i, j);
+                end = Pos { x, y };
                 25
             } else {
                 chr.to_digit(36).expect("not a b36 digit") - 10
@@ -43,5 +49,9 @@ fn parse(data: &str) -> ((usize, usize), (usize, usize), Vec<Vec<u8>>) {
         }
         grid.push(row);
     }
+    assert!(start.x < usize::MAX);
+    assert!(start.y < usize::MAX);
+    assert!(end.x < usize::MAX);
+    assert!(end.y < usize::MAX);
     (start, end, grid)
 }
